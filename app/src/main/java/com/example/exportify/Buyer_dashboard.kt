d92dcefa19cf6c5b6e1a6ff631af98e17b4686db
RanminiManditha <ranminimandithagit@gmail.com>
2023-03-14 22:25:10 +0530

    committing

---
+++ app/src/main/java/com/example/exportify/Buyer_dashboard.kt
@@ -0,0 +1,11 @@
+package com.example.exportify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Buyer_dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buyer_dashboard)
+    }
+}
