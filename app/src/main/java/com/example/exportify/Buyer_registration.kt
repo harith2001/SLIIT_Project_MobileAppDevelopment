dc629a32fd8ac3f08d97997c101126cebdc516f1
RanminiManditha <ranminimandithagit@gmail.com>
2023-03-15 21:31:01 +0530

    committing buyer registration form

---
+++ app/src/main/java/com/example/exportify/Buyer_registration.kt
@@ -0,0 +1,11 @@
+package com.example.exportify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Buyer_registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buyer_registration)
+    }
+}
